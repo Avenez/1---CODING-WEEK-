e9a88798b290b8305d0f018bdc2cbb6e75fbbfa9
Gabriele Bongio <148779911+GabrieleBongio@users.noreply.github.com>
2023-11-14 10:27:50 +0100

    Add files via upload

--- 1_CODING WEEK/Pagina quesiti/js/timer.js
+++ 1_CODING WEEK/Pagina quesiti/js/timer.js
@@ -3,7 +3,7 @@
 var width = 200,
     height = 200,
     timePassed = 0,
-    timeLimit = 60;
+    timeLimit = 5;
 
 var fields = [{
     value: timeLimit,
@@ -16,13 +16,13 @@ var fields = [{
 var nilArc = d3.svg.arc()
     .innerRadius(width / 3 - 10)
     .outerRadius(width / 3 - 10)
-    .startAngle(0)
-    .endAngle(2 * Math.PI);
+    .startAngle(2 * Math.PI)
+    .endAngle(0);
 
 var arc = d3.svg.arc()
     .innerRadius(width / 3 - 20)
     .outerRadius(width / 3 - 10)
-    .startAngle(0)
+    .startAngle(2*Math.PI)
     .endAngle(function (d) {
         return ((d.value / d.size) * 2 * Math.PI);
     });
@@ -48,7 +48,7 @@ var label = field.append("text")
     .attr("class", "label")
     .attr("dy", ".35em");
 
-(function update() {
+function update() {
 
     field
         .each(function (d) {
@@ -70,10 +70,12 @@ var label = field.append("text")
 
     if (timePassed <= timeLimit)
         setTimeout(update, 1000 - (timePassed % 1000));
-    else
-        destroyTimer();
+    //else
+    //    resetTimer();
 
-})();
+};
+
+update();
 
 function pulseText() {
     back.classed("pulse", true);
@@ -102,3 +104,158 @@ function arcTween(b) {
         return arc(i(t));
     };
 }
+
+
+const resetTimer = function(a) {
+    timePassed = 0;
+    update();
+}
+
+document.getElementById("risposta1").addEventListener("click", resetTimer);
+
+
+
+
+const questions = [
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What does CPU stand for?",
+      correct_answer: "Central Processing Unit",
+      incorrect_answers: [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      correct_answer: "Final",
+      incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "The logo for Snapchat is a Bell.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "Pointers were not used in the original C programming language; they were added later on in C++.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the most preferred image format used for logos in the Wikimedia database?",
+      correct_answer: ".svg",
+      incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "In web design, what does CSS stand for?",
+      correct_answer: "Cascading Style Sheet",
+      incorrect_answers: [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the code name for the mobile operating system Android 7.0?",
+      correct_answer: "Nougat",
+      incorrect_answers: [
+        "Ice Cream Sandwich",
+        "Jelly Bean",
+        "Marshmallow",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "On Twitter, what is the character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "Linux was first created as an alternative to Windows XP.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+  ];
+
+
+var numeroDomanda = 0;
+const nextQuestion = function () {
+    if (numeroDomanda < questions.length) {
+        document.getElementById("domanda").innerText = questions[numeroDomanda].question;   
+        if (questions[numeroDomanda].incorrect_answers.length == 3) {
+            var Numeri = []
+            NumeroRisposta = Math.floor(Math.random() * 4 + 1);
+            Numeri.push(NumeroRisposta);
+            const rispostaGiusta = document.getElementById("risposta" + NumeroRisposta);
+            rispostaGiusta.innerText = questions[numeroDomanda].correct_answer;
+            rispostaGiusta.classList.add("rispostaGiusta");
+            for (let i = 0; i<3; i++) {
+                NumeroRisposta = Math.floor(Math.random() * 4 + 1);
+                while (Numeri.includes(NumeroRisposta)) {
+                    NumeroRisposta = Math.floor(Math.random() * 4 + 1);
+                }
+                Numeri.push(NumeroRisposta);
+                const rispostaSbagliata = document.getElementById("risposta" + NumeroRisposta);
+                rispostaSbagliata.innerText = questions[numeroDomanda].incorrect_answers[i];
+            }
+        } else {
+            var Numeri = []
+            NumeroRisposta = Math.floor(Math.random() * 2 + 1);
+            Numeri.push(NumeroRisposta);
+            const rispostaGiusta = document.getElementById("risposta" + NumeroRisposta);
+            rispostaGiusta.innerText = questions[numeroDomanda].correct_answer;
+            rispostaGiusta.classList.add("rispostaGiusta");
+            if (Numeri.includes(1)) {
+                NumeroRisposta = 2;
+            } else {
+                NumeroRisposta = 1;
+            }
+            const rispostaSbagliata = document.getElementById("risposta" + NumeroRisposta);
+            rispostaSbagliata.innerText = questions[numeroDomanda].incorrect_answers[0];
+        }
+        numeroDomanda += 1;
+    } else {
+        document.getElementById("Results").value = results;
+        document.getElementById("hiddenResults").submit();
+    }
+} 
+
